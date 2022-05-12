433e94ea6601dcfdbd3f0c75ca1ca5e4411a562a
Kevinthesun456 <kevin.sun@student.allenisd.org>
2022-05-11 21:28:56 -0500

    operator generator

--- Operator.java
+++ Operator.java
@@ -1,3 +1,40 @@
 public class Operator {
+
+
+
+    public String getRandom(String[] operators){
+
+        
+    Double a = .1
+    Double b = .2
+    Double c = .3
+    
+        for(String s: operators){
+            if(operators[s] == "+"){
+                a -= .05;
+             } else if(operators[s] == "-"){
+                 b -= .05;
+             } else (operators[s] == "*"){
+                 c -= .05;
+             }
+
+
+
+            n = math.random();
+           if n < 0.1 {
+               return "/";
+           } else if n < 0.2{ 
+               return "+";
+           }else if n < 0.3{ 
+               return "-";
+           }else { 
+               return "*";
+           }
+        
+            
+           
+        
+
+    }
     
 }
